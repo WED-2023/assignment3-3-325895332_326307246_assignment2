12166a09f8d57b52c3f5ce6bb49b74efaeee087a
ShahafHT <shahafuny@gmail.com>
2025-06-12 18:31:18 +0300

    added server_domain (local \ domain) in config.js

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const state = {
+  server_domain: 'https://localhost:3000',
+  // server_domain: 'shahafr.cs.bgu.ac.il',
+}
